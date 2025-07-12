e247e8bc2bb9619c1273f1f1a63a5b3727a21872
greenbi1979 <greenbi1979@naver.com>
2025-07-12 21:49:16 +0900

    update-5

--- Test_0712.m
+++ Test_0712.m
@@ -6,11 +6,12 @@ import sltest.testmanager.*
 projectPath = fullfile(pwd, 'HEV_SeriesParallel.prj');
 openProject(projectPath);
 
-% 2. 테스트 파일 경로
+% 2. 테스트 파일 로드
 testFilePath = fullfile(pwd, 'Scripts_Data', 'HEV_MODEL_TestManager.mldatx');
+sltest.testmanager.load(testFilePath);  % <- 파일 로드만 함
 
-% 3. 테스트 실행
-resultSet = sltest.testmanager.run(testFilePath);
+% 3. 테스트 실행 (전체 활성된 테스트 실행)
+resultSet = sltest.testmanager.run();  % <- 인자 없음!
 
 % 4. 결과 저장 폴더 생성
 resultFolder = fullfile(pwd, 'Test Result');
@@ -20,7 +21,7 @@ end
 
 % 5. 결과 저장
 sltest.testmanager.exportResults(resultSet, fullfile(resultFolder, 'HEV_MODEL_TestResult.mldatx'));
-sltest.testmanager.exportResults(resultSet, fullfile(resultFolder, 'HEV_MODEL_TestResult.xml'), 'ExportFormat', 'JUnit');
+%sltest.testmanager.exportResults(resultSet, fullfile(resultFolder, 'HEV_MODEL_TestResult.xml'), 'ExportFormat', 'JUnit');
 
-% 6. 후처리
+% 6. 클린업
 sltest.testmanager.clear;
